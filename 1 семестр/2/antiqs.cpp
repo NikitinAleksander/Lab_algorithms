8725c89172da0a665a15b11b60a05aec46b7970b
NikitinAleksander <sashanikitin22@gmail.com>
2019-02-08 23:05:22 +0300

    Анти quick sort

---
+++ 1 семестр/2/antiqs.cpp
@@ -0,0 +1,28 @@
+﻿#include <iostream> 
+#include <vector> 
+#include <algorithm> 
+#include <fstream> 
+
+using namespace std;
+
+int main() {
+	ifstream input("antiqs.in");
+	ofstream output("antiqs.out");
+	int size;
+	input >> size;
+
+	vector<int> am(size);
+
+	for (int i = 0; i < size; i++) {
+		am[i] = i + 1;
+	}
+
+	for (int i = 2; i < size; i++) {
+		swap(am[i], am[i / 2]);
+	}
+
+	for (int i = 0; i < size; i++) {
+		output << am[i] << ' ';
+	}
+
+}
