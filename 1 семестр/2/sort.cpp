7da865f94f70c35ab9f3c5f55d178c2ee9b6b91d
NikitinAleksander <sashanikitin22@gmail.com>
2019-02-08 23:07:42 +0300

    Сортировка

---
+++ 1 семестр/2/sort.cpp
@@ -0,0 +1,60 @@
+﻿#include <iostream> 
+#include <algorithm> 
+#include <vector> 
+#include <ctime> 
+#include <fstream> 
+using namespace std; 
+
+int randPart(int left, int right) { 
+srand(time(NULL)); 
+return (left + rand() % (right - left + 1)); 
+} 
+
+void quickSort(vector<int> &arr, int left, int right){ 
+int i = left, j = right; 
+int pivot = arr[randPart(left, right)]; 
+
+while (i <= j) { 
+while (arr[i] < pivot) { 
+i++; 
+} 
+
+while (arr[j] > pivot) { 
+j--; 
+} 
+
+if (i <= j) { 
+swap(arr[i], arr[j]); 
+i++; 
+j--; 
+} 
+} 
+
+if (left < j) { 
+quickSort(arr, left, j); 
+} 
+if (i < right) { 
+quickSort(arr, i, right); 
+} 
+} 
+
+int main() { 
+ifstream input("sort.in"); 
+ofstream output("sort.out"); 
+
+int size; 
+input >> size; 
+
+
+vector<int> am(size); 
+for (int i = 0; i < size; i++) { 
+input >> am[i]; 
+} 
+size--; 
+
+quickSort(am, 0, size); 
+
+for (int i = 0; i <= size; i++) { 
+output << am[i] << ' '; 
+} 
+}
